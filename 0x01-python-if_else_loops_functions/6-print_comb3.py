6296f302ee05020c82b3e088a7d0afc7b53d51f8
tugume-peter <simontugume2@gmail.com>
2023-01-04 01:34:28 -0800

    combinations of natural numbers

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 for i in range(0, 10):
-    for j in range(i, 10):
-        if j == 9 and i == 9:
-            break
+    for j in range(i + 1, 10):
         if j == 9 and i == 8:
             print("{}{}".format(i, j))
         else:
