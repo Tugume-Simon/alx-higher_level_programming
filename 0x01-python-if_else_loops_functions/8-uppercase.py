a0bbd1682733f60e2c3520c5a8f2242ff3a21bb1
tugume-peter <simontugume2@gmail.com>
2023-01-04 02:11:33 -0800

    uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for i in range(len(str)):
+        num = ord(str[i]);
+        if num >= 97 and num <= 122:
+            num = num - 32
+        print("{:c}".format(num), end="")
+    print("");
+
+
