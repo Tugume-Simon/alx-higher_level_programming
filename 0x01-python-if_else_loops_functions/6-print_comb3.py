135d2951241de7b810d4f9e7f73f92e18d1e367a
tugume-peter <simontugume2@gmail.com>
2023-01-04 01:27:43 -0800

    combination of natural digits

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+for i in range(0, 10):
+    for j in range(i, 10):
+        if j == 9 and i == 9:
+            break
+        if j == 9 and i == 8:
+            print("{}{}".format(i, j))
+        else:
+            print("{}{}".format(i, j), end=", ")
