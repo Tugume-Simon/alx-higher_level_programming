65351b1c252d7a24de5159be3cd4a20ac9891ec4
tugume-peter <simontugume2@gmail.com>
2023-01-05 23:05:40 -0800

    commandline args

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
+"""
+    Program handles basic math operations
+    of addition, subtraction, multiplication
+    and division on values 10 and 5
+"""
 if __name__ == "__main__":
-    from calculator_1 import add, sub, mul, div
+from calculator_1 import add, sub, mul, div
     a = 10
     b = 5
     print("{:d} + {:d} = {:d}".format(a, b, add(10, 5)))
--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 from sys import argv
-argc = len(argv) - 1
-print("{} argument".format(argc), end="")
-if argc != 1:
-    print("s", end="")
-if argc == 0:
-    print(".")
-else:
-    print(":")
-    for i in range(1, len(argv)):
-        print("{}: {}".format(i, argv[i]))
+if __name__ == "__main__":
+    argc = len(argv) - 1
+    print("{} argument".format(argc), end="")
+    if argc != 1:
+        print("s", end="")
+    if argc == 0:
+        print(".")
+    else:
+        print(":")
+        for i in range(1, len(argv)):
+            print("{}: {}".format(i, argv[i]))
