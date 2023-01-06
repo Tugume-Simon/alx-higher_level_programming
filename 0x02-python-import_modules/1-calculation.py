ffa7225d76beca0db167a1dcbeefec92944fe67b
tugume-peter <simontugume2@gmail.com>
2023-01-05 22:30:18 -0800

    first toolbox

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -6,4 +6,4 @@ if __name__ == "__main__":
     print("{:d} + {:d} = {:d}".format(a, b, add(10, 5)))
     print("{:d} - {:d} = {:d}".format(a, b, sub(10, 5)))
     print("{:d} * {:d} = {:d}".format(a, b, mul(10, 5)))
-    print("{:d} / :d{} = {:d}".format(a, b, div(10, 5)))
+    print("{:d} / {:d} = {:d}".format(a, b, div(10, 5)))
