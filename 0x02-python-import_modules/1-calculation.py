e160985806e3c685c9273f75020138efc3fe4905
tugume-peter <simontugume2@gmail.com>
2023-01-09 07:15:17 -0800

    calculation

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -3,11 +3,8 @@ from calculator_1 import add, sub, mul, div
 
 a = 10
 b = 5
-def main(a, b):
+if __name__ == "__main__":
     print("{:d} + {:d} = {:d}".format(a, b, add(10, 5)))
     print("{:d} - {:d} = {:d}".format(a, b, sub(10, 5)))
     print("{:d} * {:d} = {:d}".format(a, b, mul(10, 5)))
     print("{:d} / {:d} = {:d}".format(a, b, div(10, 5)))
-    
-if __name__ == "__main__":
-    main(a, b);
