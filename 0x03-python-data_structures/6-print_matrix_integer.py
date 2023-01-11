697ffc5483bdb92c5cc78809d15d98d619948270
tugume-peter <simontugume2@gmail.com>
2023-01-11 07:08:41 -0800

    matrix

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    if len(matrix) > 0:
+        for row in matrix:
+            for i in row:
+                if row.index(i) != 2:
+                    print("{:d}".format(i), end=" ")
+                else:
+                    print("{:d}".format(i), end="")
+            print("")
