fae00ad92dc6f9f87147c36b1ba27440d65ac3f7
tugume-peter <simontugume2@gmail.com>
2023-01-13 01:15:19 -0800

    search and replace

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    new_list = my_list.copy()
+    while new_list.count(search) > 0:
+        idx = new_list.index(search)
+        new_list[idx] = replace
+    return new_list
+    
