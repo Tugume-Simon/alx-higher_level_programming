cea7609a15633c98432fb2686382b7361add8ee6
tugume-peter <simontugume2@gmail.com>
2023-01-13 21:30:05 -0800

    maximum from dictionary

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary is None:
+        return None
+    max = 0
+    for item in a_dictionary.items():
+        if item[1] > max:
+            max = item[1]
+    return max
