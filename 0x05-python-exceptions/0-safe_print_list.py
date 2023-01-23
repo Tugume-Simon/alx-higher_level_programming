607be5a68ebe37e6a70d958299d4e32e8f0f706d
tugume-peter <simontugume2@gmail.com>
2023-01-23 04:33:38 -0800

    safe print

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
         while i < x:
             print("{}".format(my_list[i]), end="")
             i = i + 1
-    except:
+    except IndexError:
         pass
     finally:
         print("")
