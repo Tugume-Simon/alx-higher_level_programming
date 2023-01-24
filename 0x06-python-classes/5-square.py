e2aaad8907c8220c947b3744c67670240109981c
tugume-peter <simontugume2@gmail.com>
2023-01-24 06:37:00 -0800

    printing a square

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -56,8 +56,10 @@ class Square:
         the # characters are used
 
         """
-
-        for i in range(self.__size):
-            for j in range(self.__size):
-                print("#", end="")
+        if self.__size == 0:
             print("")
+        else:
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print("#", end="")
+                print("")
