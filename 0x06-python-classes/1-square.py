037526d72a863e5e7d021f1dd9800df098b10ef3
tugume-peter <simontugume2@gmail.com>
2023-01-24 05:05:53 -0800

    square class

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""class: Module defines a class Square
+
+The Square class is defined here which will be used to
+create square objects.
+"""
+
+
+class Square():
+    """make square class
+
+    The size of the square is intentionally made private
+    because many things depend on it so it must not be
+    changed
+
+    Attributes:
+        size (int): size of the square
+
+    """
+
+    def __init__(self, size):
+        self.__size = size
