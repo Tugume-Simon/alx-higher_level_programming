ab1028db6bb14194bb3cc546f6391fb584e06cbf
tugume-peter <simontugume2@gmail.com>
2023-01-25 01:29:47 -0800

    square coordinates

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -93,6 +93,8 @@ class Square:
         if self.__size == 0:
             print("")
         else:
+            for z in range(self.__position[1]):
+                print("")
             for i in range(self.__size):
                 for x in range(self.__position[0]):
                     print(" ", end="")
