91cd3050f190e4fde8925ed62a0795b132b447b7
tugume-peter <simontugume2@gmail.com>
2023-01-27 04:11:28 -0800

    add integers' test

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""The function add_integer is defined.
+It adds two integers and/or floats.
+
+Params:
+    a (int, float)
+    b (int, float)
+
+Return:
+    sum of 'a' and 'b'
+"""
+
+
+def add_integer(a, b=98):
+    """adds two integers or decimals i.e. a & b
+
+    a and b are casted to integers
+    before performing the operation
+
+    if there's a failure, a TypeError
+    exception is raised with a message
+    'a must be an integer' or 'b must be
+    an integer'
+
+    Returns: sum of a & b
+    """
+
+    if type(a) not in [int, float]:
+        raise TypeError('a must be an integer')
+    if type(b) not in [int, float]:
+        raise TypeError('b must be an integer')
+
+    return (a + b)
