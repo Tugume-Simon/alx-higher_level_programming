36fffcaf36936ec7b3522400a167b843aef4a588
tugume-peter <simontugume2@gmail.com>
2023-01-30 04:23:02 -0800

    print_symbol correction

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -84,7 +84,7 @@ class Rectangle:
         if self.__width != 0 and self.__height != 0:
             for i in range(self.__height):
                 for j in range(self.__width):
-                    ret_str += "#"
+                    ret_str += str(self.print_symbol)
                 if i != self.__height - 1:
                     ret_str += "\n"
 
