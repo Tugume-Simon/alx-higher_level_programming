e4eea016ed557c74152b975efd4832c8122f9c26
tugume-peter <simontugume2@gmail.com>
2023-02-06 08:56:28 -0800

    same class or inherit from

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""<function> `is_kind_of_class`"""
+
+
+def is_kind_of_class(obj, a_class):
+    """tests if a given object is an instance of a given class"""
+
+    return isinstance(obj, a_class)
