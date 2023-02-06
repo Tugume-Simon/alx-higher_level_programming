247faeb0dd32aa85cc5b9dca8eb4bf157af93532
tugume-peter <simontugume2@gmail.com>
2023-02-06 05:42:46 -0800

    my list class

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""<class 'MyList'>"""
+
+
+class MyList(list):
+    """MyList class
+
+    instantiates the list class it inherits and
+    adds one new method print_sorted
+
+    Attributes:
+        none
+
+    Methods:
+        __init__ - instantiates the class plus base
+        class(es)
+        print_sorted - prints the list in ascending
+        order
+    """
+
+    def __init__(self):
+        super().__init__()
+
+    def print_sorted(self):
+        """prints a list in ascending order (sorted)"""
+        self.sort()
+        print(self)
