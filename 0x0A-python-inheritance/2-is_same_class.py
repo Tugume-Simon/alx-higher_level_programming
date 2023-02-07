27afbf14ebc334730f762f6bd6178ef9e5537c40
tugume-peter <simontugume2@gmail.com>
2023-02-06 17:58:33 -0800

    exact class of

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -12,12 +12,6 @@ def is_same_class(obj, a_class):
     Return: True if test is successful, otherwise false
     """
 
-    #name = a_class.__name__
-    #cls = obj.__class__
-    #return isinstance(obj, a_class) and issubclass(cls, a_class)\
-        #and hasattr(obj, "__{}__".format(name))
-    #return issubclass(cls, a_class) and hasattr(obj, "__{}__".format(name))
-
     if type(obj) == a_class:
         return True
     else:
