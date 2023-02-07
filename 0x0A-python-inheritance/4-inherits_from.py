1c91f4a72e9508d8aa5f3f4224827c40bd0dc75e
tugume-peter <simontugume2@gmail.com>
2023-02-06 18:12:01 -0800

    only subclass of

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""<function `inherits_from`>"""
+
+def inherits_from(obj, a_class):
+    """check to see if an object is derived from a class
+
+    Args:
+        obj (any): object
+        a_class: class
+
+    Return: True if true, otherwise False
+    """
+
+    cls = obj.__class__
+    return isinstance(obj, a_class)
