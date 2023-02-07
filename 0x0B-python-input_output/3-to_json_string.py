74151829dbcc07ca1b843bbb8a2d2a722b5a6b21
tugume-peter <simontugume2@gmail.com>
2023-02-07 06:12:43 -0800

    JSON

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
 """This module returns JSON representation of objects"""
-json = __import__('json')
+import json
 
 
 def to_json_string(my_obj):
+    """Returns JSON representation of objects
+
+    Args:
+        my_obj (obj): any
+    """
+
     return json.dumps(my_obj)
