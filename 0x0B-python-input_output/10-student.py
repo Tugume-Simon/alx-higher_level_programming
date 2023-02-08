f972f203fa7c777f9d0e9cf8f9c2dacf3ab04e4c
tugume-peter <simontugume2@gmail.com>
2023-02-08 00:46:06 -0800

    Student to JSON with filter

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""<class 'Student'>"""
+
+
+class Student:
+    """This class defines a student
+
+    Attributes:
+        first_name (str): first name
+        last_name (str): last name
+        age (int): student age
+
+    """
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        attribs = ['first_name', 'last_name', 'age']
+        if type(attrs) is list and [type(attr) is str for attr in attrs]:
+            return {key: getattr(self, key) for key in attrs if key in attribs}
+        return self.__dict__
