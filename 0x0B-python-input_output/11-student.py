c71e78bd6a49d87bfef38210071038c8abe35001
tugume-peter <simontugume2@gmail.com>
2023-02-08 01:29:54 -0800

    Student to JSON with reloat

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -24,6 +24,9 @@ class Student:
         return self.__dict__
 
     def reload_from_json(self, json):
-        self.first_name = json['first_name']
-        self.last_name = json['last_name']
-        self.age = json['age']
+        try:
+            self.first_name = json['first_name']
+            self.last_name = json['last_name']
+            self.age = json['age']
+        except Exception as e:
+            pass
