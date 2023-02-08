a03a7bd580658a864876401a71d38a83edd6aa57
tugume-peter <simontugume2@gmail.com>
2023-02-07 22:20:45 -0800

    class to JSON

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Dictionary description of objects"""
+json = __import__('json')
+
+
+def class_to_json(obj):
+    """Serializes a given object in JSON
+
+    Description: The function uses the dictionary
+    description with simple data structure (list,
+    integer, dictionary, string and boolean) that
+    it turns object to.
+
+    Args:
+        obj (obj): instance of a class
+
+    Return: dictionary description
+    """
+
+    return obj.__dict__
