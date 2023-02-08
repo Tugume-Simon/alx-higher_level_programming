dcbce74521f8c21a2fa73fd150b166f1c380f08c
tugume-peter <simontugume2@gmail.com>
2023-02-08 04:27:15 -0800

    append after

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -12,7 +12,7 @@ def append_after(filename="", search_string="", new_string=""):
         new_string (str): string to insert
     """
 
-    if len(filename) > 0:
+    try:
         with open(filename, 'r+', encoding='utf-8') as f:
             lines = f.readlines()
             count = 0
@@ -25,3 +25,5 @@ def append_after(filename="", search_string="", new_string=""):
         with open(filename, 'w', encoding='utf-8') as g:
             lines = "".join(lines)
             g.write(lines)
+    except Exception:
+        pass
