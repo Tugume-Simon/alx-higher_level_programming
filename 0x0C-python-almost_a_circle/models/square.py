6c86ea972d2c715c84961e0584092b33e1e88743
Tugume Simon Peter <simontugume2@gmail.com>
2023-02-10 06:06:57 -0800

    update square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -18,6 +18,32 @@ class Square(Rectangle):
         """
         super().__init__(size, size, x, y, id)
 
+    @property
+    def size(self):
+        """Getter for the square size"""
+        return self.width
+
+    @size.setter
+    def size(self, val):
+        """set the size of the square side"""
+        self.width = val
+        self.height = val
+
     def __str__(self):
         """String representation of square object"""
         return f'[Square] ({self.id}) {self.x}/{self.y} - {self.width}'
+
+    def update(self, *args, **kwargs):
+        """update of square object"""
+
+        if len(args) > 0:
+            attribs = ['id', 'size', 'x', 'y']
+            for n in range(len(args)):
+                if attribs[n] == 'id' and type(args[n]) is not int:
+                    raise TypeError("id must be an integer")
+                setattr(self, attribs[n], args[n])
+        else:
+            for key, value in kwargs.items():
+                if key == 'id' and type(value) is not int:
+                    raise TypeError("id must be an integer")
+                setattr(self, key, value)
